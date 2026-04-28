f5981b1478f77a8ea33cb0881c66722f7ca047a1
AndreCast1 <153152029+AndreCast1@users.noreply.github.com>
2026-04-27 23:50:27 -0500

    Agregamos el POST

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
+
+// POST
+// https://proyecto-software-api-login.onrender.com/api/auth/login - LOGIN
+// https://proyecto-software-api-login.onrender.com/api/auth/register - REGISTER
